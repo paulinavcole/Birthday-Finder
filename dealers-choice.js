9e87e59a2e0f96f140e5e2e5be62e8f18bda4a6d
Paulina Cole <lina.cole@gmail.com>
2022-04-30 18:50:33 -0400

    added basic format of website

---
+++ dealers-choice.js
@@ -0,0 +1,16 @@
+const header = document.getElementsByTagName("header")[0];
+
+document.addEventListener("scroll", function () {
+    let scrollPosition = window.scrollY;
+    if (scrollPosition > 65) { header.classList.add("active"); } 
+    else { header.classList.remove("active"); } 
+});
+
+let currentImage = 0;
+const images = document.querySelectorAll(".carousel-image");
+setInterval(() => {
+     images[currentImage].classList.add("hidden");
+     if (currentImage === 11) { currentImage = 0; } 
+     else { currentImage += 1; }
+     images[currentImage].classList.remove("hidden");
+     }, 6000);
